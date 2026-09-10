8d5da0e94c1691a4538d6674f80595e8654409df
Tialli <tialli.lara@escola.pr.gov.br>
2026-09-10 19:50:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao)
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
@@ -119,6 +119,11 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
+}
+
+function aleatorio(lista) {
+   const posicao = Math.random()* lista.length;
+   return lista[posicao];
 }
 
 mostraPergunta();
